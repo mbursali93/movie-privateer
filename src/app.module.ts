d12abf022ac93f82c463463a301e96feacf004c8
Mustafa Bursali <m.bursali16@gmail.com>
2024-01-28 16:45:21 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { config } from 'dotenv';
+
+@Module({
+  imports: [ConfigModule.forRoot({ load: [config], isGlobal: true })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
